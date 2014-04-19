7ba3cb5090e0238fdc92f5a65ed67e93765b8101
Samantha Yeargin <slyeargin@gmail.com>
2014-04-19 13:35:36 -0500

    Working, but ugly, version of game

--- app/js/source/main.js
+++ app/js/source/main.js
@@ -6,25 +6,21 @@
   var clock;
   var timer;
   var numbers;
-  // var numCards;
-  // var backCards;
+  var flipCount = 0;
 
   function init() {
     $('#start').click(startGame);
-    $('#game td').click(reveal);
-    // $('.flipper').click(compareCards);
-    // numCards = $('.card').length;
   }
 
   function startGame() {
         generateArray();
-        shuffleArray(numbers);
+        shuffleArray();
+        loadArray();
 
         clearInterval(timer);
         clock = $('#clock').data('time')*1;
         timer = setInterval(updateClock,1000);
-        // loadArray();
-       // timer();
+        $('#game td').click(reveal);
       }
 
   function generateArray(){
@@ -37,40 +33,37 @@
       }
     }
 
-  function shuffleArray(array) {
-      for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-      }
-      return array;
+  function shuffleArray() {
+    for (var i = numbers.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = numbers[i];
+      numbers[i] = numbers[j];
+      numbers[j] = temp;
     }
-
-  function reveal(){
-    var row = $(this).parent().index();
-    var col = $(this).index();
-    var pos = (row * 4) + col;
-    var img = numbers[pos];
-
-    $(this).find('.back').css('background-image', 'url(<img src="./media/card_' + img + '.png" data-card="' + img + '">)');
-    $(this).find('.flipper').addClass('rotate');
-    $(this).addClass('show');
-    checkMatch();
-
   }
 
-  // function loadArray() {
-  //   var $back = $('.back');
-  //   var count = $back.length;
-  //
-  //   for (var i = 0; i < count; i++){
-  //     var image = '<img src="./media/card_' + backCards[i] + '.png" data-card="' + backCards[i] + '">';
-  //     var whichBack = $back[i];
-  //     $(whichBack).append(image);
-  //   }
-  //
-  // }
+  function reveal(){
+    $(this).find('.flipper').addClass('rotate');
+
+    if ($('.rotate').length < 2){
+      flipCount++;
+    } else {
+      flipCount++;
+      checkMatch();
+    }
+  }
+
+  function loadArray() {
+      var $back = $('.back');
+      var count = $back.length;
+
+      for (var i = 0; i < count; i++){
+        var image = '<img src="./media/card_' + numbers[i] + '.png">';
+        var whichBack = $back[i];
+        $(whichBack).append(image);
+      }
+
+    }
 
   function updateClock(){
     clock--;
@@ -101,71 +94,27 @@
   }
 
   function checkMatch() {
-    var $matches = $('.show');
+    var $matches = $('.rotate > .back > img');
+    var img1 = $matches.eq(0).attr('src');
+    var img2 = $matches.eq(1).attr('src');
 
-    if ($matches.length === 2){
-      var td1 = $matches[0];
-      var td2 = $matches[1];
+    console.log($matches);
+    console.log(img1);
+    console.log(img2);
 
-      var img1 = $(td1).find('.back').css('background-image');
-      var img2 = $(td2).find('.back').css('background-image');
-
+    if (flipCount === 2){
       if (img1 === img2){
-        $matches.addClass('match');
+        $('.rotate').addClass('match');
+        $('.rotate').removeClass('rotate');
         $matches.off('click');
       } else {
-        setTimeout(function() {
-          $matches.find('.flipper').removeClass('rotate');
-          //setTimeout(function() {
-        //     $matches.find('.back').css('background-image', '');
-        //
-        // }, 1000);
-        }, 1000);
+        setTimeout(function(){
+          $('.rotate').removeClass('rotate');
+        }, 400);
       }
-      $matches.removeClass('show');
+      flipCount = 0;
     }
   }
-  // function timer() {
-  // revealBoard()
-  // countdown()
-  // closeBoard()  if numCards = flippedCardCount you win!
-  // }
 
-
-  // function compareCards(){
-  //   var lastFlip = 0;
-  //   var flippedCardCount = ('.flipper .rotate').length;
-  //
-  //   if (this.isClass('matched')) {
-  //     return;
-  //   }
-  //
-  //
-  //
-  //   if (flippedCardCount <= 1) {
-  //     $(this).toggleClass('rotate');
-  //
-  //     var $img = $('.flipper .rotate > .back .matched');
-  //
-  //     $('.flipper .rotate > .back:not(.matched)').addClass('matched');
-  //     if ($('.flipper .rotate > .back .matched').data('img') === lastFlip) {
-  //       $img.addClass('flipped');
-  //       lastFlip = 0;
-  //     } else {
-  //       lastFlip = $img.data;
-  //     }
-  //
-  //
-  //   } else {
-  //     $(this).removeClass('rotate');
-  //     lastFlip = 0;
-  //   }
-
-
-    // var lastFlip = 0;
-
-
-//
-// }
 
 }) ();
